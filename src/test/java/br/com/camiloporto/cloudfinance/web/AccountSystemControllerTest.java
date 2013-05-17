88575ecb3b954d3d463da283b97ef65ba6fd34a1
camiloporto <camiloporto@gmail.com>
2013-05-17 12:59:16 -0300

    configurado esquema de testes web mvc. porem ta dando um erro ao subir o soring context web

---
+++ src/test/java/br/com/camiloporto/cloudfinance/web/AccountSystemControllerTest.java
@@ -0,0 +1,32 @@
+package br.com.camiloporto.cloudfinance.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+@ContextConfiguration(locations = {"classpath:/META-INF/spring/applicationContext*.xml", "webmvc-config.xml"})
+@WebAppConfiguration
+@ActiveProfiles("unit-test")
+public class AccountSystemControllerTest extends AbstractTestNGSpringContextTests {
+	
+	@Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @BeforeMethod
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+    
+	@Test
+	public void shouldInsertNewAccountSystem() {
+	}
+}
