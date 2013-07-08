76272dfe459ecc1cf77f7fc71cc997110ba69a52
camiloporto <camiloporto@gmail.com>
2013-07-08 16:28:11 -0300

    adicionadas validacoes para recuperar extrato de conta

--- src/test/java/br/com/camiloporto/cloudfinance/service/AccountStatementManagerTest.java
+++ src/test/java/br/com/camiloporto/cloudfinance/service/AccountStatementManagerTest.java
@@ -1,22 +1,31 @@
 package br.com.camiloporto.cloudfinance.service;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.Iterator;
 import java.util.List;
 
+import javax.validation.ConstraintViolationException;
+
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import br.com.camiloporto.cloudfinance.AbstractCloudFinanceDatabaseTest;
 import br.com.camiloporto.cloudfinance.builders.ProfileBuilder;
 import br.com.camiloporto.cloudfinance.checkers.AccountStatementChecker;
+import br.com.camiloporto.cloudfinance.checkers.ExceptionChecker;
 import br.com.camiloporto.cloudfinance.model.Account;
 import br.com.camiloporto.cloudfinance.model.AccountNode;
 import br.com.camiloporto.cloudfinance.model.AccountTransaction;
 import br.com.camiloporto.cloudfinance.model.Profile;
 import br.com.camiloporto.cloudfinance.service.impl.AccountStatement;
+import br.com.camiloporto.cloudfinance.service.impl.AccountStatementManagerImpl;
 import br.com.camiloporto.cloudfinance.service.impl.TransactionManagerImpl;
 
 public class AccountStatementManagerTest extends AbstractCloudFinanceDatabaseTest {
@@ -34,6 +43,8 @@ public class AccountStatementManagerTest extends AbstractCloudFinanceDatabaseTes
 	private Account incomes;
 	private Account bank;
 	private Account outgoings;
+	
+	private Clock fakeClock = Mockito.mock(Clock.class);
 	
 	private Account root;
 
@@ -59,6 +70,7 @@ public class AccountStatementManagerTest extends AbstractCloudFinanceDatabaseTes
 	@BeforeMethod
 	public void beforeTests() {
 		cleanUserData();
+		((AccountStatementManagerImpl)accountStatementService).setClock(fakeClock);
 		final String camiloporto = "some@email.com";
 		final String senha = "1234";
 		Profile p = new ProfileBuilder()
@@ -129,7 +141,7 @@ public class AccountStatementManagerTest extends AbstractCloudFinanceDatabaseTes
 	}
 	
 	@Test
-	public void shouldCalculateAccountBalanceBeforeTheIntervalInformed() {
+	public void shouldCalculateAccountBalanceBeforeTheGivenInterval() {
 		AccountStatement accountStatement = accountStatementService.getAccountStatement(profile, bank.getId(), tx02Date.getTime(), tx03Date.getTime());
 		new AccountStatementChecker()
 			.forStatement(accountStatement)
@@ -137,7 +149,7 @@ public class AccountStatementManagerTest extends AbstractCloudFinanceDatabaseTes
 	}
 	
 	@Test
-	public void shouldCalculateAccountBalanceAfterTheIntervalInformed() {
+	public void shouldCalculateAccountBalanceAfterTheGivenInterval() {
 		AccountStatement accountStatement = accountStatementService.getAccountStatement(profile, bank.getId(), tx02Date.getTime(), tx03Date.getTime());
 		new AccountStatementChecker()
 			.forStatement(accountStatement)
@@ -145,7 +157,7 @@ public class AccountStatementManagerTest extends AbstractCloudFinanceDatabaseTes
 	}
 	
 	@Test
-	public void shouldCalculateAccountOperationalBalanceOfTheIntervalInformed() {
+	public void shouldCalculateAccountOperationalBalanceOfTheGivenInterval() {
 		AccountStatement accountStatement = accountStatementService.getAccountStatement(profile, bank.getId(), tx02Date.getTime(), tx03Date.getTime());
 		new AccountStatementChecker()
 			.forStatement(accountStatement)
@@ -153,11 +165,75 @@ public class AccountStatementManagerTest extends AbstractCloudFinanceDatabaseTes
 	}
 	
 	@Test
-	public void shouldGetEntriesInInformedTimeInterval() {
+	public void shouldGetEntriesInGivenInterval() {
 		AccountStatement accountStatement = accountStatementService.getAccountStatement(profile, bank.getId(), tx02Date.getTime(), tx04Date.getTime());
 		new AccountStatementChecker()
 			.forStatement(accountStatement)
 			//							date	desc		value			src/dest
 			.assertStatementIsPresent(tx02Date.getTime(), "t2", tx02Amount.negate(), outgoings);
+	}
+	
+	@Test(dataProvider = "getAccountStatement.InvalidInputs")
+	public void shouldThrowsConstraintViolationExceptionWhenInvalidInputForGetAccountStatement(
+			Profile profile, Long accountId, Date begin, Date end, String expectedErrorMessage) {
+		try {
+			accountStatementService.getAccountStatement(profile, accountId, begin, end);
+		} catch (ConstraintViolationException e) {
+			e.printStackTrace();
+			new ExceptionChecker(e)
+				.assertExpectedErrorCountIs(1)
+				.assertContainsMessageTemplate(expectedErrorMessage);
+		}
+	}
+	
+	@DataProvider(name = "getAccountStatement.InvalidInputs")
+	public Iterator<Object[]> getAccountStatementInvalidInputData() {
+		beforeTests();
+		List<Object[]> testData = new ArrayList<Object[]>();
+		
+		// no profile
+		testData.add(new Object[] { null, bank.getId(), tx01Date.getTime(), tx03Date.getTime(), "br.com.camiloporto.cloudfinance.user.LOGGED_USER_REQUIRED" });
+
+		// no account id informed
+		testData.add(new Object[] { profile, null, tx01Date.getTime(), tx03Date.getTime(), "br.com.camiloporto.cloudfinance.report.statement.ACCOUNT_REQUIRED" });
+
+		// begin date > end date
+		testData.add(new Object[] {profile, bank.getId(), tx03Date.getTime(), tx01Date.getTime(), "br.com.camiloporto.cloudfinance.report.statement.BEGIN_DATE_GREATER_THAN_END_DATE"});
+		
+		return testData.iterator();
+	}
+	
+	@Test
+	public void shouldSetDefaultBeginDateIfItNotGiven() {
+		
+		//assuming today is 14/06/2013
+		Calendar fakeToday = new GregorianCalendar(2013, Calendar.JUNE, 14);
+		Mockito.when(fakeClock.today()).thenReturn(fakeToday);
+				
+		Date beginDate = null;
+		AccountStatement accountStatement = accountStatementService.getAccountStatement(profile, bank.getId(), beginDate, tx03Date.getTime());
+		
+		//expected Begin Date should be 11/06/2013 (3 days before 'today')
+		new AccountStatementChecker()
+			.forStatement(accountStatement)
+			.assertBalanceBefore(new BigDecimal("1000.0"));
+		
+	}
+	
+	@Test
+	public void shouldSetDefaultEndDateIfItNotGiven() {
+		
+		//assuming today is 14/06/2013
+		Calendar fakeToday = new GregorianCalendar(2013, Calendar.JUNE, 14);
+		Mockito.when(fakeClock.today()).thenReturn(fakeToday);
+				
+		Date endDate = null;
+		AccountStatement accountStatement = accountStatementService.getAccountStatement(profile, bank.getId(), tx02Date.getTime(), endDate);
+		
+		//expected End Date should be 'today'
+		new AccountStatementChecker()
+			.forStatement(accountStatement)
+			.assertBalanceBefore(new BigDecimal("1000.0"));
+		
 	}
 }
