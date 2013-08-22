e6661200722936161edad992444b968462da5607
camiloporto <camiloporto@gmail.com>
2013-08-22 17:07:04 -0300

    refatorado testes WUI. reusando codigos de start e end das suites de testes para subir e descer o webdriver e algumas tarefas basicas para outros testes

---
+++ src/test/java/br/com/camiloporto/cloudfinance/ui/mobile/AbstractWUITest.java
@@ -0,0 +1,48 @@
+package br.com.camiloporto.cloudfinance.ui.mobile;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+public class AbstractWUITest {
+	
+	public static final String NEWUSER_PASS = "s3cret";
+	public static final String NEWUSER_GMAIL_COM = "newuser@gmail.com";
+	
+	protected WebDriver driver;
+	private boolean userCreated = false;
+	
+	@BeforeClass
+	public void createNewUser() {
+		if(!userCreated) {
+			startWebDriver();
+			MobileHomePage mhp = PageFactory.initElements(driver,
+					MobileHomePage.class);
+			mhp.clickNewUserProfileLink();
+			MobileNewUserPage newUserPage = PageFactory.initElements(driver,
+					MobileNewUserPage.class);
+			newUserPage.fillNewUserForm(NEWUSER_GMAIL_COM, NEWUSER_PASS,
+					NEWUSER_PASS).submit();
+			closeWebDriver();
+			userCreated = true;
+		}
+	}
+	
+	@BeforeMethod
+	public void startWebDriver() {
+		driver = new HtmlUnitDriver();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.get("http://localhost:8080/cloudfinance/mobile");
+	}
+	
+	@AfterMethod
+	public void closeWebDriver() {
+		driver.close();
+	}
+
+}
--- src/test/java/br/com/camiloporto/cloudfinance/ui/mobile/NewUserProfileMobileWUITest.java
+++ src/test/java/br/com/camiloporto/cloudfinance/ui/mobile/NewUserProfileMobileWUITest.java
@@ -1,31 +1,9 @@
 package br.com.camiloporto.cloudfinance.ui.mobile;
 
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.support.PageFactory;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class NewUserProfileMobileWUITest {
-	
-	private WebDriver driver;
-	
-	@BeforeMethod
-	public void startWebDriver() {
-		driver = new HtmlUnitDriver();
-		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-		driver.get("http://localhost:8080/cloudfinance/mobile");
-	}
-	
-	@AfterMethod
-	public void closeWebDriver() {
-		driver.close();
-	}
+public class NewUserProfileMobileWUITest extends AbstractWUITest {
 	
 	@Test
 	public void shouldCreateNewUserProfile() {
